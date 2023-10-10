6d83ae2e4ffe199ec3a878da884c1fce11d66e6f
m6z1 <sonmyungj1zz@gmail.com>
2023-10-10 21:03:49 +0900

    [FEAT] #1 회원가입 페이지로 이동

---
+++ app/src/main/java/org/sopt/dosopttemplate/LoginActivity.kt
@@ -0,0 +1,26 @@
+package org.sopt.dosopttemplate
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        goSignUp()
+    }
+
+    private fun goSignUp() = with(binding) {
+        loginTvSingUp.setOnClickListener {
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
