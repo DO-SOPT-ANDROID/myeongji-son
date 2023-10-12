3e01a7e455c1e4b32f5a1754f0e1cb035007ec62
m6z1 <sonmyungj1zz@gmail.com>
2023-10-12 11:28:20 +0900

    [REFACTOR] #1 함수 순서 변경

--- app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
@@ -57,6 +57,11 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateValues() = with(binding) {
+        loginId = loginEtId.text.toString()
+        loginPw = binding.loginEtPw.text.toString()
+    }
+
     private fun checkData() {
         if(::id.isInitialized && ::pw.isInitialized) {
             if (loginId == id && loginPw == pw) {
@@ -69,11 +74,6 @@ class LoginActivity : AppCompatActivity() {
         else {
             showToast("회원가입이 되어있지 않습니다.")
         }
-    }
-
-    private fun updateValues() = with(binding) {
-        loginId = loginEtId.text.toString()
-        loginPw = binding.loginEtPw.text.toString()
     }
 
     private fun startMainActivity() {
