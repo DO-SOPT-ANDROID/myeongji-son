0dc34e3acac8eb7b36e3364e37f476e3cc80e0c5
m6z1 <sonmyungj1zz@gmail.com>
2023-11-17 02:52:31 +0900

    add(ResponseUser): 홈 프래그먼트에 연결할 api의 응답 DTO 생성

---
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/response/ResponseUser.kt
@@ -0,0 +1,24 @@
+package org.sopt.dosopttemplate.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseUser(
+    @SerialName("data")
+    val data: List<Data>,
+)
+
+@Serializable
+data class Data(
+    @SerialName("avatar")
+    val avatar: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("first_name")
+    val first_name: String,
+    @SerialName("id")
+    val id: Int,
+    @SerialName("last_name")
+    val last_name: String,
+)
