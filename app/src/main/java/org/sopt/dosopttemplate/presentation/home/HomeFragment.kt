30a853328214d91c97a66084afbcaa39bf502dbb
m6z1 <sonmyungj1zz@gmail.com>
2023-11-17 02:53:01 +0900

    remove(HomeFragment): 과제할 부분에 있는 기존 코드 삭제

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
@@ -5,21 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ConcatAdapter
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
-import org.sopt.dosopttemplate.presentation.home.adapter.FriendAdapter
-import org.sopt.dosopttemplate.presentation.home.adapter.MyProfileAdapter
-import org.sopt.dosopttemplate.presentation.home.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
+
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "바인딩 객체가 생성되지 않음" }
-
-    private val viewModel: HomeViewModel by viewModels()
-    private var myProfileAdapter: MyProfileAdapter? = null
-    private var friendAdapter: FriendAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,27 +24,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initAdapter()
-    }
-
-    private fun initAdapter() {
-        myProfileAdapter = MyProfileAdapter(requireContext()).apply {
-            setMyProfile(viewModel.profileData)
-        }
-
-        friendAdapter = FriendAdapter(requireContext()).apply {
-            setFriendList(viewModel.mockFriends)
-        }
-
-        val concatAdapter = ConcatAdapter(myProfileAdapter, friendAdapter)
-        binding.rvFriend.adapter = concatAdapter
     }
 
     override fun onDestroyView() {
-        binding.rvFriend.adapter = null
-        myProfileAdapter = null
-        friendAdapter = null
         _binding = null
         super.onDestroyView()
     }
