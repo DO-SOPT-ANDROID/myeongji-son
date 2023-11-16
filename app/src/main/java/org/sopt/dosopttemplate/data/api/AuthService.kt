40cd01b2d69d213cd88de04462b4268406da371c
m6z1 <sonmyungj1zz@gmail.com>
2023-11-17 02:02:53 +0900

    feat(AuthService): 로그인 API 호출 메서드 생성

--- app/src/main/java/org/sopt/dosopttemplate/data/api/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/api/AuthService.kt
@@ -1,6 +1,8 @@
 package org.sopt.dosopttemplate.data.api
 
+import org.sopt.dosopttemplate.data.model.request.RequestSignIn
 import org.sopt.dosopttemplate.data.model.request.RequestSignUp
+import org.sopt.dosopttemplate.data.model.response.ResponseSignIn
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -10,4 +12,9 @@ interface AuthService {
     suspend fun postSignUp(
         @Body request: RequestSignUp,
     ): Response<Unit>
+
+    @POST("api/v1/members/sign-in")
+    suspend fun postSignIn(
+        @Body request: RequestSignIn,
+    ): Response<ResponseSignIn>
 }
