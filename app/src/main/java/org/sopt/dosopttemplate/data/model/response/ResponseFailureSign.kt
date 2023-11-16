950787b981f5e7dd0be4eda7afc52aca38695d85
m6z1 <sonmyungj1zz@gmail.com>
2023-11-16 17:34:24 +0900

    feat: 회원가입 response data 생성

---
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/response/ResponseFailureSign.kt
@@ -0,0 +1,5 @@
+package org.sopt.dosopttemplate.data.model.response
+
+data class ResponseFailureSign(
+    val message: String
+)
---
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/response/ResponseSignUp.kt
@@ -0,0 +1,5 @@
+package org.sopt.dosopttemplate.data.model.response
+
+data class ResponseSignUp(
+    val path: String
+)
